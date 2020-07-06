4b9d47d96c8f2edf0a17d843972573123656d1ad
Future <apollo_mirai@yahoo.co.jp>
2020-07-06 10:13:38 +0900

    7/6 10:13

--- train_cycle.py
+++ train_cycle.py
@@ -11,7 +11,7 @@ from evaluate_network import evaluate_network
 # デュアルネットワークの作成
 dual_network()
 
-for i in range(5):
+for i in range(1):
     print('Train',i,'====================')
     # セルフプレイ部
     self_play()
